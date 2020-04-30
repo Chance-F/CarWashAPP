0d9bd8b0570fa074703b23494c146dbf5724f0f3
Chance-F <61389310+Chance-F@users.noreply.github.com>
2020-04-30 19:17:12 -0400

    Update environment.ts

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyD0ykDAGkg3edfZVrYlHO3OazVz223KaxU",
-    authDomain: "ionicapplication-1345b.firebaseapp.com",
-    databaseURL: "https://ionicapplication-1345b.firebaseio.com",
-    projectId: "ionicapplication-1345b",
-    storageBucket: "ionicapplication-1345b.appspot.com",
-    messagingSenderId: "399972122329",
-    appId: "1:399972122329:web:560b06d32afce9dceb88c8",
-    measurementId: "G-HPW5GJSTSM"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "
   }
 
 };
